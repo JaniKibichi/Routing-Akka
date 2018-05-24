7763e6f26af400bf1e9cc607fdd5313d131857bb
janikibichi <janikibichi@gmail.com>
2018-05-24 16:37:22 +0300

    created smallest mailbox

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ This is the 'art and science' of routing messages to multiple actors using route
 - We want to send messages in a round robin order
 - We want to send the same message to all actors in a group
 - We want to dynamically distribute work based on a preconfigured pattern
+<br><br>
+- Branch out to explore creating SmallestMailBoxPool of Actors
+````
+git checkout -b create_smallest_mailbox_pool master
+````
+- Create the file to handle the actor system: <b>com.github.janikibichi.learnakka.monitoring.SmallestMailPool.scala</b>
+- Run the App to [see how to manage the mailbox.](https://asciinema.org/a/Teg6OOQ24cPFFq6GwqXNcsYAg)
---
+++ src/main/scala/com/github/janikibichi/learnakka/routing/SmallestMailPool.scala
@@ -0,0 +1,23 @@
+package com.github.janikibichi.learnakka.routing
+
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.routing.SmallestMailboxPool
+
+object SmallestMailPool extends App{
+  val actorSystem = ActorSystem("SmallMailBox")
+
+  //Create and Define an Actor
+  val router = actorSystem.actorOf(SmallestMailboxPool(5).props(Props[SmallestMailPoolActor]))
+
+  //Send Message to Actor
+  for(i <- 1 to 5){
+    router ! s"Hello $i"
+  }
+}
+
+class SmallestMailPoolActor extends Actor{
+  override def receive ={
+    case msg: String => println(s"I am ${self.path.name}")
+    case _ => println(s"I don't understand the message")
+  }
+}
